6fa97788ca4a4fb3ad70bc6d2483879e587e17a9
Crinnger <crinnger@gmail.com>
2021-04-20 15:57:23 -0400

    first commit

---
+++ src/main/java/br/com/crinnger/interbootcamp/ecommerce/checkout/InterEcommerceCheckoutApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.interbootcamp.ecommerce.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterEcommerceCheckoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterEcommerceCheckoutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crinnger/interbootcamp/ecommerce/checkout/InterEcommerceCheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crinnger.interbootcamp.ecommerce.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterEcommerceCheckoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
